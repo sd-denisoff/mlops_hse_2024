6bfc55871dfede98b2673f9a0cc6fa6c6fc07334
Vladimir <karlov.v01@gmail.com>
2024-12-01 19:41:15 +0300

    fix context in cmompose

--- create_minio_bucket.sh
+++ create_minio_bucket.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 until mc ls minio > /dev/null 2>&1; do
     sleep 0.5
 done
 
-mc mb minio/post-bucket
+mc alias set myminio http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
+mc mb myminio/${MINIO_BUCKET}
+mc ls myminio
