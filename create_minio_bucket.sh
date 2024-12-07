0f44557425e7816a93b30b965d4c1be3e3304be0
Vladimir <karlov.v01@gmail.com>
2024-12-07 20:21:53 +0300

    fix minio imit

--- create_minio_bucket.sh
+++ create_minio_bucket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+sleep 10
 until mc ls minio > /dev/null 2>&1; do
-    sleep 0.5
+    sleep 10
 done
 
 mc alias set myminio http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
