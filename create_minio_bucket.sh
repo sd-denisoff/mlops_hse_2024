b96c95eccd149202ac1f90f5f8b5ff37a0a0ee1e
Vladimir <karlov.v01@gmail.com>
2024-12-07 20:41:33 +0300

    fix minio

--- create_minio_bucket.sh
+++ create_minio_bucket.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sleep 10
-until mc ls minio > /dev/null 2>&1; do
-    sleep 10
-done
-
 mc alias set myminio http://minio:9000 ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
 mc mb myminio/${MINIO_BUCKET}
 mc ls myminio
